e505c71fcb625b3854b5c045ba367f14c305ced7
Miles <49863440+ywq880611@users.noreply.github.com>
2020-07-05 18:15:01 +0800

    Add distance API but did not check for routeID

--- src/Externel_Data_API/bus_weather_crawler.py
+++ src/Externel_Data_API/bus_weather_crawler.py
@@ -21,6 +21,10 @@ class BusWeatherCrawler:
     # Weather Information
     weather_api_url = url_keys.OW_API_URL
     weather_api_key_dict = url_keys.OW_API_KEY_DICT
+    # Route Information
+    route_api_url = url_keys.ROUTE_API_URL
+    # Distance Information
+    distance_api_url = url_keys.DISTANCE_API_URL
 
     LOGGER_NAME = 'weatherCrawlerLogger'
 
@@ -104,6 +108,59 @@ class BusWeatherCrawler:
             self.log.ERROR("request - {}: {}".format(url, e))
 
 
+    def request_route_api_coor(self, orginLat, orginLon, destinationLat, destinationLon):
+        url = BusWeatherCrawler.route_api_url.format(
+            orgin = orginLat + "," + orginLon,
+            destination = destinationLat + "," + destinationLon
+        )
+        try:
+            r = requests.get(url)
+            self.log.INFO("request route api: {}".format(url))
+
+            if r.status_code != 200 or r.json() == None:
+                self.log.WARN("request route api failed: {}".format(r.status_code))
+
+            ret = r.json()
+            self.log.INFO("response: 200 OK")
+            return ret
+        except Exception as e:
+            self.log.ERROR("request - {}: {}".format(url, e))
+
+    def request_route_api(self, orginid, destinationid):
+        orgin = self.request_busstopinfo_api(orginid)
+        destination = self.request_busstopinfo_api(destinationid)
+        oLat = orgin['results'][0]['latitude']
+        oLon = orgin['results'][0]['longitude']
+        dLat = destination['results'][0]['latitude']
+        dLon = destination['results'][0]['longitude']
+        return self.request_route_api_coor(oLat, oLon, dLat, dLon)
+
+    def request_distance_api(self, orginid, destinationid, routeid):
+        orgin = self.request_busstopinfo_api(orginid)
+        destination = self.request_busstopinfo_api(destinationid)
+        oLat = orgin['results'][0]['latitude']
+        oLon = orgin['results'][0]['longitude']
+        dLat = destination['results'][0]['latitude']
+        dLon = destination['results'][0]['longitude']
+        url = BusWeatherCrawler.distance_api_url.format(
+            orgin = oLat + "," + oLon,
+            destination = dLat + "," + dLon
+        )
+        try:
+            r = requests.get(url)
+            self.log.INFO("request route api: {}".format(url))
+
+            if r.status_code != 200 or r.json() == None:
+                self.log.WARN("request route api failed: {}".format(r.status_code))
+
+            ret = r.json()
+            self.log.INFO("response: 200 OK")
+            return int(ret['rows'][0]['elements'][0]['distance']['value'])
+        except Exception as e:
+            self.log.ERROR("request - {}: {}".format(url, e))
+
+
+
     def insert_weather_data(self, value_lst):
         # sql = """
         #     INSERT INTO {tb_name}
@@ -159,13 +216,16 @@ if __name__ == "__main__":
     crawler = BusWeatherCrawler()
 
     # get weather
-    print(crawler.request_weather_api(5.349562, -7.278198))
+    #print(crawler.request_weather_api(5.349562, -7.278198))
     # realtimebus can be request without routeid
-    print(crawler.request_realtimebus_api(46))
-    print(crawler.request_realtimebus_api(46,16))
+    #print(crawler.request_realtimebus_api(46))
+    #print(crawler.request_realtimebus_api(46,16))
     # time table
-    print(crawler.request_timetablebus_api(46,16))
+    #print(crawler.request_timetablebus_api(46,16))
     # stop info
-    print(crawler.request_busstopinfo_api(46))
-
+    #print(crawler.request_busstopinfo_api(46))
+    # route info
+    #print(crawler.request_route_api(1380,1381))
+    #distance info
+    print(crawler.request_distance_api(1380, 1381, str(17)))
     # crawler.worker()
